fd5386c13f2aa562c6cecce326d240bb2fd9f8ce
Narasimha Kamath <narasimhakamath@outlook.com>
2022-02-12 23:47:56 +0530

    - Created user schema.

---
+++ src/models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	mailAddress: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	profilePicture: {
+		type: Buffer
+	},
+	isApproved: {
+		type: Boolean,
+		default: false
+	},
+	isActive: {
+		type: Boolean,
+		default: true
+	}
+}, {
+	timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
