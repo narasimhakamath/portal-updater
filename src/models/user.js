db43be520a1ad022acaaa01640741e4a20805d6a
Narasimha Kamath <narasimhakamath@outlook.com>
2022-02-13 00:22:36 +0530

    - Added attribute: isSuperUser in the schema: company.

--- src/models/user.js
+++ src/models/user.js
@@ -22,6 +22,10 @@ const userSchema = new mongoose.Schema({
 		type: Boolean,
 		default: false
 	},
+	isSuperUser: {
+		type: Boolean,
+		default: false
+	},
 	isActive: {
 		type: Boolean,
 		default: true
