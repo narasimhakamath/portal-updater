891a9be5a3960b19485b99fa319b4ec6245e98aa
Narasimha Kamath <narasimhakamath@outlook.com>
2022-02-13 01:04:07 +0530

    - Created companyupdate schema.

---
+++ src/models/companyupdate.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const companyupdateSchema = new mongoose.Schema({
+	companyID: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: 'Company'
+	},
+	userID: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: 'User'
+	},
+	commitID: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	branchName: {
+		type: String,
+		required: true,
+		trim: true
+	}
+}, {
+	timestamps: true
+});
+
+const CompanyUpdate = mongoose.model('CompanyUpdate', companyupdateSchema);
+module.exports = CompanyUpdate;
