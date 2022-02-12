be585ccda96c6b702468946b6fa24030dbaee19c
Narasimha Kamath <narasimhakamath@outlook.com>
2022-02-13 01:20:28 +0530

    - Created a POST API: /users.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const userRoute = require('./routes/user');
+
 mongoose.connect(process.env.MONGO_URI);
 
 const app = express();
 app.use(express.json());
+
+app.use(userRoute);
 
 app.listen(process.env.PORT, () => console.log(`Listening on port: ${process.env.PORT}`));
---
+++ src/routes/user.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post(`/users`, async (req, res) => {
+
+});
+
+module.exports = router;
