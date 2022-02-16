e2ce2c885d83ec24007d9d0f48d4cb0075704afe
Narasimha Kamath <narasimhakamath@outlook.com>
2022-02-16 23:31:48 +0530

    - Created API endpoints for user management.

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,22 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post(`/users`, async (req, res) => {
+router.post(`/users/signup`, async (req, res) => {
+});
+
+router.post('/users/login', async (req, res) => {
+
+});
+
+router.post('/users/logout', async (req, res) => {
+
+});
+
+router.get('/users/profile', async (req, res) => {
+
+});
+
+router.get('/users/picture', async (req, res) => {
 
 });
 
